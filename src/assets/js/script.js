34d1ef8f60f55f494275c2a89cd423cedafea096
prince chakram <princechakram619@gmail.com>
2024-06-13 23:03:43 +0530

    first commit

---
+++ src/assets/js/script.js
@@ -0,0 +1,32 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+
+// var downArrow = document.getElementById("btn1");
+// var upArrow = document.getElementById("btn2");
+
+// downArrow.onclick = function () {
+//     'use strict';
+//     document.getElementById("first-list").style = "top:-620px";
+//     document.getElementById("second-list").style = "top:-620px";
+//     downArrow.style = "display:none";
+//     upArrow.style = "display:block";
+// };
+
+// upArrow.onclick = function () {
+//     'use strict';
+//     document.getElementById("first-list").style = "top:0";
+//     document.getElementById("second-list").style = "top:80px";
+//     upArrow.style = "display:none";
+//     downArrow.style = "display:block";
+// };
+
